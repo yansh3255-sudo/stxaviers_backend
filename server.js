540466d52be5e1d3d546065b049fa4478a487113
Ansh Yadav <yansh3255@gmail.com>
2025-11-19 17:48:06 +0530

    Update server to connect to Supabase and change port

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 import express from "express";
+import { supabase } from "./config/supabase.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("St. Xaviers ERP Backend Running Successfully!");
+app.get("/test-db", async (req, res) => {
+  const { data, error } = await supabase.from("students").select("*").limit(1);
+
+  if (error) return res.send("DB ERROR: " + error.message);
+  res.send("DB CONNECTED: " + JSON.stringify(data));
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => console.log("Server running on " + PORT));
