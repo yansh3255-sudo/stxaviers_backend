a7bebf3e453895f1a74878762922aa8e89f6ac5f
Ansh Yadav <yansh3255@gmail.com>
2025-11-19 17:30:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("St. Xaviers ERP Backend is live!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("St. Xaviers ERP Backend Running Successfully!");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
